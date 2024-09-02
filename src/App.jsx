f18d1551451bf78ab94441483799121df7c3d27d
Theinterna <yalvin58@gmail.com>
2024-09-01 22:48:33 -0700

    Add a title and the image upload component to the webpage

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ImageUpload from './ImageUpload'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <main>
-      
+      Optical Character Recognition Project with MHL AI/ML Global Hack Week
+      {/* Use the componenet here */}
+      <ImageUpload />
     </main>
   )
 }
