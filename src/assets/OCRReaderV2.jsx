5b695eab37428b900610e7ab146980e0afcc569e
Theinterna <yalvin58@gmail.com>
2024-09-02 10:33:49 -0700

    Add scaffolding for OCRReader version 2

---
+++ src/assets/OCRReaderV2.jsx
@@ -0,0 +1,13 @@
+// OCRReaderV2 component
+
+import React from "react";
+
+const OCRReaderV2 = () => {
+    return (
+        <div>
+            <h1>Hello! This is OCRReaderV2</h1>
+        </div>
+    )
+}
+
+export default OCRReaderV2;
