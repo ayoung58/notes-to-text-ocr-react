691b55b3c64e2e031bd6da03a324e3611f2e99eb
Theinterna <yalvin58@gmail.com>
2024-09-01 22:33:53 -0700

    Create an ImageUpload component for image input

--- src/ImageUpload.jsx
+++ src/ImageUpload.jsx
@@ -1,1 +1,17 @@
 // ImageUpload Component
+
+import React from 'react';
+
+// Create a function ImageUpload
+const ImageUpload = () => {
+
+    return (
+        <div>
+            {/* only accept a file input, and can only be images */}
+            <input type="file" accept="image/*" />
+        </div>
+    )
+};
+
+// from this file, we export one component: ImageUpload
+export default ImageUpload;
