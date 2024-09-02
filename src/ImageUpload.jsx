823d707cee29133e8f54464ef06697be0dab56fe
Theinterna <yalvin58@gmail.com>
2024-09-01 23:01:26 -0700

    Add preliminary code to read data of image uploaded

--- src/ImageUpload.jsx
+++ src/ImageUpload.jsx
@@ -3,12 +3,31 @@
 import React from 'react';
 
 // Create a function ImageUpload
-const ImageUpload = () => {
+// Since App.jsx will use this component, it is the parent
+// We can pass information from parent to child using the parentheses
+const ImageUpload = ({ onImageChange }) => {
 
+    // new function to handle image upload to read data of image uploaded
+    const handleImageUpload = (event) => {
+        // saves the data of the uploaded file to selectedFile
+        // without zero, then it would save data of all files, useful if can upload multiple
+        // since we can only upload one file, we want data of most recent, hence the 0
+        const selectedFile = event.target.files[0];
+
+        // if there is a file that has been uploaded
+        if (selectedFile) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                onImageChange(reader.result);
+            };
+            reader.readAsDataURL(selectedFile);
+        }
+    }
     return (
         <div>
             {/* only accept a file input, and can only be images */}
-            <input type="file" accept="image/*" />
+            {/* onChange: whenever input changes, call handleImageUpload */}
+            <input type="file" accept="image/*" onChange={handleImageUpload}/>
         </div>
     )
 };
