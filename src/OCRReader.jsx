a249c04ef244b7d0751c5fb060385d0a3d8d49de
Theinterna <yalvin58@gmail.com>
2024-09-01 23:11:46 -0700

    Set image data in OCRReader

--- src/OCRReader.jsx
+++ src/OCRReader.jsx
@@ -1,9 +1,24 @@
 // OCRReader Component
 
 import React from "react";
+import ImageUpload from "./ImageUpload";
 
 const OCRReader = () => {
+    // useState() hook: first variable is the data, second is used to change the data
+    // allows us to set the data of a variable
+    // initially, image is null, but if 1.png is uploaded, setImage is 1.png, and image is set to 1.png
+    const [image, setImage] = useState(null);
 
-}
+    const handleImageChange = (imageData) => {
+        setImage(imageData);
+    };
+
+    return (
+        <div>
+            {/* sets the parameter of ImageUpload component */}
+            <ImageUpload onImageChange={handleImageChange} />
+        </div>
+    )
+};
 
 export default OCRReader;
