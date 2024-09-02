ee5d16ea99ec46735124a24799a2ceedc54b9cd1
Theinterna <yalvin58@gmail.com>
2024-09-02 10:43:20 -0700

    Add tesseract OCR capabilities to OCRReaderV2 as well

--- src/OCRReaderV2.jsx
+++ src/OCRReaderV2.jsx
@@ -1,8 +1,26 @@
 // OCRReaderV2 component
 
 import React from "react";
+import ImageUpload from "./ImageUpload";
+import Tesseract from "tesseract.js";
 
 const OCRReaderV2 = () => {
+
+    const [image, setImage] = useState(null);
+    const [text, setText] = useState("-----------");
+    
+    const handleImageChange = ( imageData) => {
+        setImage(imageData);
+        processImage(imageData);
+    }
+
+    const processImage = (imageData) => {
+        Tesseract.recognize(imageData, "eng", {
+            logger: (m) => console.log(m)
+        }).then(({data: {text}}) => {
+            setText(text);
+        });
+    };
     return (
         <div>
             <h1>Hello! This is OCRReaderV2</h1>
